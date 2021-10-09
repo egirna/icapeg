0d526541e544161f99f2f601c6dc4e5bb4a51e62
haitham911 <haitham911eg@gmail.com>
2021-10-09 17:39:03 +0200

    test

--- s10gwloop.sh
+++ s10gwloop.sh
@@ -4,7 +4,7 @@ trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>gw.txt 2>&1
 countmain=1
 for i in $(seq $countmain); do
-count=10
+count=30
 for i in $(seq $count); do
 filerb="./reb$i.pdf"
 touch "$filerb"  && rm "$filerb" && time  c-icap-client -i 18.203.158.160  -p 1344 -s gw_rebuild  -f ./test.pdf -o "$filerb" -v &
