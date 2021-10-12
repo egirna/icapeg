0dd29fae772135cf60127e6daff9958b03c086b3
marwan <marwangalal746>
2021-10-12 10:11:13 +0200

    unit test for TransformVmrayToSubmitResponse func

---
+++ transformers/vmray_test.go
@@ -0,0 +1,66 @@
+package transformers
+
+import (
+	"icapeg/dtos"
+	"strconv"
+	"testing"
+)
+
+func TestTransformVmrayToSubmitResponse(t *testing.T) {
+	type testSample struct {
+		vsr *dtos.VmraySubmitResponse
+		submitResp *dtos.SubmitResponse
+	}
+	sampleTable := []testSample{
+		{
+			vsr: &dtos.VmraySubmitResponse{
+				Data: dtos.VmraySubmitData{
+					Submissions: []dtos.VmraySubmissions{dtos.VmraySubmissions{
+						SubmissionID: 1,
+						SubmissionSampleID: 2,
+					}, dtos.VmraySubmissions{
+						SubmissionID: 3,
+						SubmissionSampleID: 4,
+					},dtos.VmraySubmissions{
+						SubmissionID: 5,
+						SubmissionSampleID: 6,
+					}},
+				},
+			},
+			submitResp: &dtos.SubmitResponse{
+				SubmissionID: strconv.Itoa(1),
+				SubmissionSampleID: strconv.Itoa(2),
+				SubmissionExists: true,
+			},
+		},
+		{
+			vsr: &dtos.VmraySubmitResponse{
+				Data: dtos.VmraySubmitData{
+					Submissions: []dtos.VmraySubmissions{},
+				},
+			},
+			submitResp: &dtos.SubmitResponse{
+				SubmissionExists: false,
+			},
+		},
+	}
+	for _, sample := range sampleTable {
+		got := TransformVmrayToSubmitResponse(sample.vsr)
+		want := sample.submitResp
+		if got.SubmissionExists != want.SubmissionExists {
+
+		}
+		if got.SubmissionExists != want.SubmissionExists {
+			t.Errorf("TransformVirusTotalToSubmitResponse Failed for %s , wanted: %v got: %v",
+				"SubmissionExists", want.SubmissionExists, got.SubmissionExists)
+		}
+		if got.SubmissionExists == want.SubmissionExists && got.SubmissionID != want.SubmissionID {
+			t.Errorf("TransformVirusTotalToSampleInfo Failed for %s , wanted: %s got: %s",
+				"SubmissionID", want.SubmissionID, got.SubmissionID)
+		}
+		if got.SubmissionExists == want.SubmissionExists && got.SubmissionSampleID != want.SubmissionSampleID {
+			t.Errorf("TransformVirusTotalToSampleInfo Failed for %s , wanted: %s got: %s",
+				"SubmissionExists", want.SubmissionSampleID, got.SubmissionSampleID)
+		}
+	}
+}
