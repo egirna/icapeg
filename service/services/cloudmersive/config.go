569cf9f3b12a671b5ca2765c27b57a249fbec8bd
RahmaYasser <rahma.y9079@gmail.com>
2022-08-02 15:34:57 +0200

    added function to initialize cloudmersive service

--- service/services/cloudmersive/config.go
+++ service/services/cloudmersive/config.go
@@ -25,8 +25,7 @@ type CloudMersive struct {
 	bypassExts                  []string
 	processExts                 []string
 	rejectExts                  []string
-	ScanUrl                     string
-	ReportUrl                   string
+	ScanEndPoint                string
 	Timeout                     time.Duration
 	APIKey                      string
 	FailThreshold               int
@@ -34,4 +33,31 @@ type CloudMersive struct {
 	returnOrigIfMaxSizeExc      bool
 	return400IfFileExtRejected  bool
 	generalFunc                 *general_functions.GeneralFunc
+}
+
+func NewCloudMersiveService(serviceName, methodName string, httpMsg *utils.HttpMsg) *CloudMersive {
+	return &CloudMersive{
+		httpMsg:                     httpMsg,
+		serviceName:                 serviceName,
+		methodName:                  methodName,
+		allowExecutables:            cloudMersiveConfig.allowExecutables,
+		allowEmlExternalEntities:    cloudMersiveConfig.allowEmlExternalEntities,
+		allowMacros:                 cloudMersiveConfig.allowMacros,
+		allowScripts:                cloudMersiveConfig.allowScripts,
+		allowInvalidFiles:           cloudMersiveConfig.allowInvalidFiles,
+		allowPasswordProtectedFiles: cloudMersiveConfig.allowPasswordProtectedFiles,
+		restrictFileTypes:           cloudMersiveConfig.restrictFileTypes,
+		maxFileSize:                 cloudMersiveConfig.maxFileSize,
+		bypassExts:                  cloudMersiveConfig.bypassExts,
+		processExts:                 cloudMersiveConfig.processExts,
+		rejectExts:                  cloudMersiveConfig.rejectExts,
+		ScanEndPoint:                cloudMersiveConfig.ScanEndPoint,
+		Timeout:                     cloudMersiveConfig.Timeout * time.Second,
+		APIKey:                      cloudMersiveConfig.APIKey,
+		FailThreshold:               cloudMersiveConfig.FailThreshold,
+		policy:                      cloudMersiveConfig.policy,
+		returnOrigIfMaxSizeExc:      cloudMersiveConfig.returnOrigIfMaxSizeExc,
+		return400IfFileExtRejected:  cloudMersiveConfig.return400IfFileExtRejected,
+		generalFunc:                 general_functions.NewGeneralFunc(httpMsg),
+	}
 }
