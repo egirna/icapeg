b8af23e17612676d465b0a4163c64e69f8e38498
RahmaYasser <rahma.y9079@gmail.com>
2022-08-09 17:42:43 +0200

    debugging SendFileToAPI

--- service/services/cloudmersive/cloudmersive.go
+++ service/services/cloudmersive/cloudmersive.go
@@ -4,13 +4,56 @@ import (
 	"bytes"
 	"context"
 	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"icapeg/utils"
 	"io"
+	"io/ioutil"
 	"mime/multipart"
 	"net/http"
 	"strconv"
 )
+
+func (c CloudMersive) Processing(partial bool) (int, interface{}, map[string]string) {
+	//TODO implement me
+
+	// no need to scan part of the file, this service needs all the file at one time
+	if partial {
+		return utils.Continue, nil, nil
+	}
+
+	//extracting the file from http message
+	file, _, err := c.generalFunc.CopyingFileToTheBuffer(c.methodName)
+	if err != nil {
+		return utils.InternalServerErrStatusCodeStr, nil, nil
+	}
+	// comparing file extension with restrictFileTypes list
+
+	filename := c.generalFunc.GetFileName()
+	fmt.Println("filename: ", filename)
+	//TODO add file size check
+	serviceResp, err := c.SendFileToAPI(file, filename)
+	if err != nil {
+		fmt.Println("error 55555\n", err.Error())
+		return serviceResp.StatusCode, nil, nil
+	}
+	body, err := ioutil.ReadAll(serviceResp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return serviceResp.StatusCode, nil, nil
+	}
+	fmt.Println(serviceResp.StatusCode)
+	fmt.Println(serviceResp.Header)
+	fmt.Println(string(body))
+	// process file response, compare response json with request headers value
+	var data map[string]interface{}
+	json.Unmarshal(body, &data) // Convert JSON data into interface{} type
+	fmt.Println("data after encoding: \n", data)
+	fmt.Println("----------------------------")
+	fmt.Println(data["CleanResult"])
+	// processing headers
+	return serviceResp.StatusCode, nil, nil
+}
 
 func (c *CloudMersive) SendFileToAPI(f *bytes.Buffer, filename string) (*http.Response, error) {
 	url := c.BaseURL + c.ScanEndPoint
