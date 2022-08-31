0ea6428441fe68a5f5baa3e01240991e59e3ac22
RahmaYasser <rahma.y9079@gmail.com>
2022-08-31 16:06:57 +0200

    init service function

--- service/services/grayimages/config.go
+++ service/services/grayimages/config.go
@@ -2,6 +2,7 @@ package grayimages
 
 import (
 	"icapeg/http-message"
+	"icapeg/readValues"
 	services_utilities "icapeg/service/services-utilities"
 	general_functions "icapeg/service/services-utilities/general-functions"
 	"sync"
@@ -27,4 +28,18 @@ type Grayimages struct {
 	returnOrigIfMaxSizeExc     bool
 	return400IfFileExtRejected bool
 	generalFunc                *general_functions.GeneralFunc
+}
+
+func InitGrayimagesConfig(serviceName string) {
+	doOnce.Do(func() {
+		grayimagesConfig = &Grayimages{
+			maxFileSize:                readValues.ReadValuesInt(serviceName + ".max_filesize"),
+			bypassExts:                 readValues.ReadValuesSlice(serviceName + ".bypass_extensions"),
+			processExts:                readValues.ReadValuesSlice(serviceName + ".process_extensions"),
+			rejectExts:                 readValues.ReadValuesSlice(serviceName + ".reject_extensions"),
+			returnOrigIfMaxSizeExc:     readValues.ReadValuesBool(serviceName + ".return_original_if_max_file_size_exceeded"),
+			return400IfFileExtRejected: readValues.ReadValuesBool(serviceName + ".return_400_if_file_ext_rejected"),
+		}
+		grayimagesConfig.extArrs = services_utilities.InitExtsArr(grayimagesConfig.processExts, grayimagesConfig.rejectExts, grayimagesConfig.bypassExts)
+	})
 }
