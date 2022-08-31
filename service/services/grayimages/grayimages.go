57345ab5487d9f7c13a3b141d2fc3880558860ea
RahmaYasser <rahma.y9079@gmail.com>
2022-08-31 17:16:30 +0200

    added service congigration

--- service/services/grayimages/grayimages.go
+++ service/services/grayimages/grayimages.go
@@ -2,7 +2,9 @@ package grayimages
 
 import (
 	"bytes"
+	utils "icapeg/consts"
 	"io"
+	"io/ioutil"
 	"log"
 	"mime/multipart"
 	"net/http"
@@ -10,9 +12,100 @@ import (
 	"time"
 )
 
-func (g Grayimages) Processing(b bool) (int, interface{}, map[string]string) {
-	//TODO implement me
-	panic("implement me")
+func (g *Grayimages) Processing(partial bool) (int, interface{}, map[string]string) {
+	log.Println("processing")
+	serviceHeaders := make(map[string]string)
+
+	// no need to scan part of the file, this service needs all the file at ine time
+	if partial {
+		return utils.Continue, nil, nil
+	}
+
+	isGzip := false
+
+	//extracting the file from http message
+	file, reqContentType, err := g.generalFunc.CopyingFileToTheBuffer(g.methodName)
+	if err != nil {
+		return utils.InternalServerErrStatusCodeStr, nil, nil
+	}
+
+	//getting the extension of the file
+	var contentType []string
+	if len(contentType) == 0 {
+		contentType = append(contentType, "")
+	}
+	var fileName string
+	if g.methodName == utils.ICAPModeReq {
+		contentType = g.httpMsg.Request.Header["Content-Type"]
+		fileName = g.generalFunc.GetFileName()
+	} else {
+		contentType = g.httpMsg.Response.Header["Content-Type"]
+		fileName = g.generalFunc.GetFileName()
+	}
+	if len(contentType) == 0 {
+		contentType = append(contentType, "")
+	}
+	fileExtension := g.generalFunc.GetMimeExtension(file.Bytes(), contentType[0], fileName)
+
+	/*isProcess, icapStatus, httpMsg := g.generalFunc.CheckTheExtension(fileExtension, g.extArrs,
+		g.processExts, g.rejectExts, g.bypassExts, g.return400IfFileExtRejected, isGzip,
+		g.serviceName, g.methodName, GrayImagesIdentifier, g.httpMsg.Request.RequestURI, reqContentType, file)
+	if !isProcess {
+		return icapStatus, httpMsg, serviceHeaders
+	}*/
+
+	if fileExtension != "png" && fileExtension != "jpeg" && fileExtension != "jpg" && fileExtension != "webp" {
+		originalFile := file.Bytes()
+		if isGzip {
+			originalFile, err = g.generalFunc.CompressFileGzip(originalFile)
+			if err != nil {
+				return utils.InternalServerErrStatusCodeStr, nil, serviceHeaders
+			}
+		}
+		originalFile = g.generalFunc.PreparingFileAfterScanning(originalFile, reqContentType, g.methodName)
+		return utils.NoModificationStatusCodeStr, g.generalFunc.ReturningHttpMessageWithFile(g.methodName, originalFile), serviceHeaders
+	}
+	// sending request to gray images api
+	scannedFile := file.Bytes()
+	serviceResp, err := g.SendFileToAPI(file, fileExtension, fileName)
+	if err != nil || serviceResp.StatusCode != 200 {
+		if serviceResp != nil {
+			log.Println(serviceResp.StatusCode)
+			log.Println(serviceResp.Body)
+		}
+		// if file recompress the file if it was compressed
+		if isGzip {
+			scannedFile, err = g.generalFunc.CompressFileGzip(scannedFile)
+			if err != nil {
+				return utils.InternalServerErrStatusCodeStr, nil, serviceHeaders
+			}
+		}
+		// send file with no modification code
+		scannedFile = g.generalFunc.PreparingFileAfterScanning(scannedFile, reqContentType, g.methodName)
+		return utils.NoModificationStatusCodeStr, g.generalFunc.ReturningHttpMessageWithFile(g.methodName, scannedFile), serviceHeaders
+	}
+	// get the byte array from response body
+	scannedFile, err = ioutil.ReadAll(serviceResp.Body)
+	if err != nil {
+		if isGzip {
+			scannedFile, err = g.generalFunc.CompressFileGzip(scannedFile)
+			if err != nil {
+				return utils.InternalServerErrStatusCodeStr, nil, serviceHeaders
+			}
+		}
+		scannedFile = g.generalFunc.PreparingFileAfterScanning(scannedFile, reqContentType, g.methodName)
+		return utils.NoModificationStatusCodeStr, g.generalFunc.ReturningHttpMessageWithFile(g.methodName, scannedFile), serviceHeaders
+	}
+	// compress file again if it's decompressed
+	if isGzip {
+		scannedFile, err = g.generalFunc.CompressFileGzip(scannedFile)
+		if err != nil {
+			return utils.InternalServerErrStatusCodeStr, nil, serviceHeaders
+		}
+	}
+	// send the image after gray scale
+	scannedFile = g.generalFunc.PreparingFileAfterScanning(scannedFile, reqContentType, g.methodName)
+	return utils.OkStatusCodeStr, g.generalFunc.ReturningHttpMessageWithFile(g.methodName, scannedFile), serviceHeaders
 }
 
 func (g *Grayimages) SendFileToAPI(f *bytes.Buffer, fileType string, fileName string) (*http.Response, error) {
