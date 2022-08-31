b72e4d716795bab3a5de963b9169e9345b58eead
RahmaYasser <rahma.y9079@gmail.com>
2022-08-31 16:13:47 +0200

    implemented service functions

---
+++ service/services/grayimages/grayimages.go
@@ -0,0 +1,11 @@
+package grayimages
+
+func (g Grayimages) Processing(b bool) (int, interface{}, map[string]string) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (g Grayimages) ISTagValue() string {
+	//TODO implement me
+	panic("implement me")
+}
