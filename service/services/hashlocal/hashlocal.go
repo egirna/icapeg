99be09e16d9031ecb1132db44335e029e3ed809e
Ola_Mamdouh234 <ola274249@gmail.com>
2024-01-04 16:25:58 +0200

    Edit hashlocal.go

--- service/services/hashlocal/hashlocal.go
+++ service/services/hashlocal/hashlocal.go
@@ -3,11 +3,10 @@ package hashlocal
 import (
 	"bufio"
 	"bytes"
-	"context"
 	"crypto/sha256"
 	"crypto/subtle"
 	"encoding/hex"
-	"encoding/json"
+
 	"fmt"
 	utils "icapeg/consts"
 	"icapeg/logging"
@@ -258,15 +257,8 @@ func (h *Hashlocal) sendFileToScan(f *bytes.Buffer) (bool, error) {
 	h.FileHash = pass
 	//  the file path
 	filePath := "./Datalocal/datalocal.txt"
-	req, err := http.NewRequest("GET", h.ScanUrl+pass, nil)
-	client := &http.Client{}
-	ctx, cancel := context.WithTimeout(context.Background(), h.Timeout)
-	defer cancel()
-	req = req.WithContext(ctx)
-	resp, err := client.Do(req)
-	if err != nil {
-		return false, err
-	}
+	//req, err := http.NewRequest("GET", h.ScanUrl+pass, nil)
+
 	// Check if the target value is present in the file
 	found, err := checkValueInFile(filePath, pass)
 	if err != nil {
@@ -281,19 +273,10 @@ func (h *Hashlocal) sendFileToScan(f *bytes.Buffer) (bool, error) {
 	log.SetOutput(logfile)
 	if found {
 		log.Printf("Value '%s' not found in the file.\n", pass)
+		return false, nil
 
 	} else {
 		log.Printf("Value '%s' not found in the file.\n", pass)
-
-	}
-
-	defer resp.Body.Close()
-	var data map[string]interface{}
-	err = json.NewDecoder(resp.Body).Decode(&data)
-	y, err := (fmt.Sprint(data["KnownMalicious"])), nil
-	if len(y) > 0 && y != "<nil>" {
-		return true, nil
-	} else {
 		return false, nil
 
 	}
